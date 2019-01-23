7916a53454c5d8a608b43980786eee2facbebe76
Anthony Daff <anthony@inoutput.io>
2019-01-23 16:57:32 +1100

    test: refactor tests to follow a more standard structure
    Converted tests with multiple Acts to separate tests (kept those with single Act and multiple Assert as singular).
    Shifted transformation tests to a separate file.

--- YarraTrams.Havm2TramTracker/YarraTrams.Havm2TramTracker.Tests/Havm2TramTrackerParserTests.cs
+++ YarraTrams.Havm2TramTracker/YarraTrams.Havm2TramTracker.Tests/Havm2TramTrackerParserTests.cs
@@ -39,12 +39,12 @@ namespace YarraTrams.Havm2TramTracker.Tests
 
             // assert
             Assert.IsTrue(trips.Count == tripsDT.Rows.Count, "Number of records in DataTable ({1:d}) doesn't match number of records in Trip class list ({0:d}).", trips.Count, tripsDT.Rows.Count);
-            //Todo: check more stuff. Everything!
+            //Todo: check more stuff. Everything! Only once we've settled on the data contract.
             Assert.IsTrue(trips[0].HastusTripId == tripsDT[0].TripID, "TripId field in DataTable ({1}) doesn't match HastusTripId from Trip class ({0}).", trips[0].HastusTripId, tripsDT[0].TripID);
         }
 
         [TestMethod]
-        public void TestJsonToTrips()
+        public void TestJsonToTripsWithStandardJson()
         {
             // arrange
             #region "JSON string containing a single trip"
@@ -134,90 +134,202 @@ namespace YarraTrams.Havm2TramTracker.Tests
             var trips = Processor.Processor.CopyJsonToTrips(jsonString);
 
             // assert
-            Assert.IsTrue(trips.Count == 1, "Number of records in Trip class list ({1:d}) doesn't match number of records in the JSON ({0:d}).",1, trips.Count);
+            Assert.IsTrue(trips.Count == 1, "Number of records in Trip class list ({1:d}) doesn't match number of records in the JSON ({0:d}).", 1, trips.Count);
             //Todo: check more stuff
-            Assert.IsTrue(trips[0].Stops[trips[0].Stops.Count-1].HastusStopId == "1124", "We expected the HastusStopId on the final stop of the first trip to be '1124' but it appears to be '{0}' instead.", trips[0].Stops[trips[0].Stops.Count - 1].HastusStopId);
-        }
-        //Todo Test for malformed AND extra fields (to be ignored) and missing fields.
-        [TestMethod]
-        public void TestAtLayoverTransformation()
-        {//Todo: split that in to 5
-            var headwayMapping = new Dictionary<int, short>
-            {
-                { -1, 0 },          //Negatives go to zero
-                { 60, 1 },          // Normal, no rounding
-                { 90, 2 },          // Round up
-                { 29, 0 },          // Round down
-                {32768*60,32767}    //Anything exceeding a short gets set to the max short
-            };
-
-            foreach (var map in headwayMapping)
-            {
-                // arrange
-                var trip = new Models.HavmTrip
-                {
-                    HeadwayNextSeconds = map.Key //Ignoring all other properties
-                };
-
-                // act
-                var AtLayover = Models.Transformations.GetAtLayovertime(trip);
-
-                // assert
-                Assert.IsTrue(AtLayover == map.Value, "Expecting value {0} from input of {1} but got {2} instead.", map.Value, map.Key, AtLayover);
-            }
+            Assert.IsTrue(trips[0].Stops[trips[0].Stops.Count - 1].HastusStopId == "1124", "We expected the HastusStopId on the final stop of the first trip to be '1124' but it appears to be '{0}' instead.", trips[0].Stops[trips[0].Stops.Count - 1].HastusStopId);
         }
 
         [TestMethod]
-        public void TestUpDirectionTransformationWithValidInputs()
-        {
-            var directionMapping = new Dictionary<string, Nullable<bool>>
-            {
-                { "UP", true },        //UP is true
-                { " down ", false },   // DOWN is false, even with padding and lower-case
-            };
-
-            foreach (var map in directionMapping)
-            {
-                // arrange
-                var trip = new Models.HavmTrip
-                {
-                    Direction = map.Key //Ignoring all other properties
-                };
-
-                // act
-                var UpDirection = Models.Transformations.GetUpDirection(trip);
-                
-                // assert
-                Assert.IsTrue(UpDirection == map.Value, "Expecting value {0} from input of {1} but got {2} instead.", map.Value, map.Key, UpDirection);
-            }
-        }
-
-        [TestMethod]
-        public void TestUpDirectionTransformationWithBadInput()
+        public void TestJsonToTripsWithExtraFieldInJson()
         {
             // arrange
-            var trip = new Models.HavmTrip
-            {
-                Direction = "all over the place" //Ignoring all other properties
-            };
-
+            #region "JSON string containing a single trip and more fields than we expect"
+            var jsonString = @"
+                            [
+                                {
+                                    ""ANewFieldThatWeDoNotKnowAbout"": ""A new value that we are not expecting"",
+                                    ""hastusTripId"": 62770458,
+                                    ""block"": ""gh02- 27 This is wrong"",
+                                    ""displayCode"": ""61"",
+                                    ""startTimepoint"": ""ebtn"",
+                                    ""startTime"": ""00:00:00"",
+                                    ""endTimepoint"": ""scj1"",
+                                    ""endTime"": ""00:06:00"",
+                                    ""headwayNextSeconds"": 0,
+                                    ""nextDisplayCode"": ""61"",
+                                    ""direction"": ""DOWN"",
+                                    ""vehicleType"": ""b"",
+                                    ""distanceMetres"": 3074,
+                                    ""isPublic"": true,
+                                    ""operationalDay"": ""2018-04-23T00:00:00"",
+                                    ""stops"": [
+                                        {
+                                            ""passingTime"": ""00:00:00"",
+                                            ""hastusStopId"": ""1135"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:01:00"",
+                                            ""hastusStopId"": ""1134"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:01:00"",
+                                            ""hastusStopId"": ""1133"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:02:00"",
+                                            ""hastusStopId"": ""1132"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:02:00"",
+                                            ""hastusStopId"": ""1131"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:03:00"",
+                                            ""hastusStopId"": ""1130"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:03:00"",
+                                            ""hastusStopId"": ""1129"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:04:00"",
+                                            ""hastusStopId"": ""1128"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:04:00"",
+                                            ""hastusStopId"": ""1127"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:05:00"",
+                                            ""hastusStopId"": ""1126"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:05:00"",
+                                            ""hastusStopId"": ""1125"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:06:00"",
+                                            ""hastusStopId"": ""1124"",
+                                            ""isMonitoredOPRReliability"": false
+                                        }
+                                    ]
+                                }
+                            ]";
+            #endregion
             // act
-            bool UpDirection;
-            bool success;
-            try
-            {
-                UpDirection = Models.Transformations.GetUpDirection(trip);
-                success = false;
-            }
-            #pragma warning disable CS0168 // Variable is declared but never used
-            catch (FormatException ex)
-            #pragma warning restore CS0168 // Variable is declared but never used
-            {
-                success = true; //we are expecting an exception when we pass invalid input
-            }
+            var trips = Processor.Processor.CopyJsonToTrips(jsonString);
 
             // assert
-            Assert.IsTrue(success, "Expecting a FormatException when passing invalid input. Didn't encounter such an exception.");
+            Assert.IsTrue(trips.Count == 1, "Number of records in Trip class list ({1:d}) doesn't match number of records in the JSON ({0:d}).", 1, trips.Count);
+            //Todo: check more stuff
+            Assert.IsTrue(trips[0].Stops[trips[0].Stops.Count - 1].HastusStopId == "1124", "We expected the HastusStopId on the final stop of the first trip to be '1124' but it appears to be '{0}' instead.", trips[0].Stops[trips[0].Stops.Count - 1].HastusStopId);
+        }
+
+        [TestMethod]
+        public void TestJsonToTripsWithMissingFieldInJson()
+        {
+            // arrange
+            #region "JSON string containing a single trip but missing an important field"
+            var jsonString = @"
+                            [
+                                {
+                                    //Usually the Hastus Trip Id field is here
+                                    ""block"": ""gh02- 27 This is wrong"",
+                                    ""displayCode"": ""61"",
+                                    ""startTimepoint"": ""ebtn"",
+                                    ""startTime"": ""00:00:00"",
+                                    ""endTimepoint"": ""scj1"",
+                                    ""endTime"": ""00:06:00"",
+                                    ""headwayNextSeconds"": 0,
+                                    ""nextDisplayCode"": ""61"",
+                                    ""direction"": ""DOWN"",
+                                    ""vehicleType"": ""b"",
+                                    ""distanceMetres"": 3074,
+                                    ""isPublic"": true,
+                                    ""operationalDay"": ""2018-04-23T00:00:00"",
+                                    ""stops"": [
+                                        {
+                                            ""passingTime"": ""00:00:00"",
+                                            ""hastusStopId"": ""1135"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:01:00"",
+                                            ""hastusStopId"": ""1134"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:01:00"",
+                                            ""hastusStopId"": ""1133"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:02:00"",
+                                            ""hastusStopId"": ""1132"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:02:00"",
+                                            ""hastusStopId"": ""1131"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:03:00"",
+                                            ""hastusStopId"": ""1130"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:03:00"",
+                                            ""hastusStopId"": ""1129"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:04:00"",
+                                            ""hastusStopId"": ""1128"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:04:00"",
+                                            ""hastusStopId"": ""1127"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:05:00"",
+                                            ""hastusStopId"": ""1126"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:05:00"",
+                                            ""hastusStopId"": ""1125"",
+                                            ""isMonitoredOPRReliability"": false
+                                        },
+                                        {
+                                            ""passingTime"": ""00:06:00"",
+                                            ""hastusStopId"": ""1124"",
+                                            ""isMonitoredOPRReliability"": false
+                                        }
+                                    ]
+                                }
+                            ]";
+            #endregion
+            // act
+            var trips = Processor.Processor.CopyJsonToTrips(jsonString);
+
+            // assert
+            //Todo: check that derialisation fails in some way
+            Assert.IsTrue(trips.Count == 1, "Number of records in Trip class list ({1:d}) doesn't match number of records in the JSON ({0:d}).", 1, trips.Count);
+            Assert.IsTrue(trips[0].Stops[trips[0].Stops.Count - 1].HastusStopId == "1124", "We expected the HastusStopId on the final stop of the first trip to be '1124' but it appears to be '{0}' instead.", trips[0].Stops[trips[0].Stops.Count - 1].HastusStopId);
         }
     }
 }
---
+++ YarraTrams.Havm2TramTracker/YarraTrams.Havm2TramTracker.Tests/Havm2TramTrackerTransformationTests.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YarraTrams.Havm2TramTracker.Models;
+using YarraTrams.Havm2TramTracker.Processor;
+
+namespace YarraTrams.Havm2TramTracker.Tests
+{
+    [TestClass]
+    public class Havm2TramTrackerTransformationTests
+    {
+
+        #region AtLayover
+        [TestMethod]
+        public void TestAtLayoverTransformationWithNegative()
+        {
+            // arrange
+            const int headway = -1; //Seconds
+            const short expectedResult = 0; //Minute
+            var trip = new Models.HavmTrip
+            {
+                HeadwayNextSeconds = headway //Ignoring all other properties
+            };
+
+            // act
+            var atLayover = Models.Transformations.GetAtLayovertime(trip);
+
+            // assert
+            Assert.IsTrue(atLayover == expectedResult, "Expecting value {0} from input of \"{1}\" but got {2} instead.", expectedResult, headway, atLayover);
+        }
+
+        [TestMethod]
+        public void TestAtLayoverTransformationWithRoundFigure()
+        {
+            // arrange
+            //Todo: Confirm rounding rule with YT
+            const int headway = 60; //Seconds
+            const short expectedResult = 1; //Minute
+            var trip = new Models.HavmTrip
+            {
+                HeadwayNextSeconds = headway //Ignoring all other properties
+            };
+
+            // act
+            var atLayover = Models.Transformations.GetAtLayovertime(trip);
+
+            // assert
+            Assert.IsTrue(atLayover == expectedResult, "Expecting value {0} from input of \"{1}\" but got {2} instead.", expectedResult, headway, atLayover);
+        }
+
+        [TestMethod]
+        public void TestAtLayoverTransformationWithFigureToRoundDown()
+        {
+            // arrange
+            const int headway = 29; //Seconds
+            const short expectedResult = 0; //Minute
+            var trip = new Models.HavmTrip
+            {
+                HeadwayNextSeconds = headway //Ignoring all other properties
+            };
+
+            // act
+            var atLayover = Models.Transformations.GetAtLayovertime(trip);
+
+            // assert
+            Assert.IsTrue(atLayover == expectedResult, "Expecting value {0} from input of \"{1}\" but got {2} instead.", expectedResult, headway, atLayover);
+        }
+
+        [TestMethod]
+        public void TestAtLayoverTransformationWithFigureToRoundUp()
+        {
+            // arrange
+            const int headway = 90; //Seconds
+            const short expectedResult = 2; //Minute
+            var trip = new Models.HavmTrip
+            {
+                HeadwayNextSeconds = headway //Ignoring all other properties
+            };
+
+            // act
+            var atLayover = Models.Transformations.GetAtLayovertime(trip);
+
+            // assert
+            Assert.IsTrue(atLayover == expectedResult, "Expecting value {0} from input of \"{1}\" but got {2} instead.", expectedResult, headway, atLayover);
+        }
+
+        [TestMethod]
+        public void TestAtLayoverTransformationWithFigureOutside2BitRange()
+        {
+            // arrange
+            const int headway = 32768 * 60; //Seconds
+            const short expectedResult = 32767; //Minute
+            var trip = new Models.HavmTrip
+            {
+                HeadwayNextSeconds = headway //Ignoring all other properties
+            };
+
+            // act
+            var atLayover = Models.Transformations.GetAtLayovertime(trip);
+
+            // assert
+            Assert.IsTrue(atLayover == expectedResult, "Expecting value {0} from input of \"{1}\" but got {2} instead.", expectedResult, headway, atLayover);
+        }
+        #endregion
+
+        #region UpDirection
+        [TestMethod]
+        public void TestUpDirectionTransformationWithValidUp()
+        {
+            // arrange
+            const string direction = "UP";
+            var trip = new Models.HavmTrip
+            {
+                Direction = direction //Ignoring all other properties
+            };
+
+            // act
+            var UpDirection = Models.Transformations.GetUpDirection(trip);
+
+            // assert
+            Assert.IsTrue(UpDirection, "Expecting value {0} from input of \"{1}\" but got {2} instead.", true, direction, UpDirection);
+
+        }
+
+        [TestMethod]
+        public void TestUpDirectionTransformationWithValidDown()
+        {
+            // arrange
+            const string direction = " down ";
+            var trip = new Models.HavmTrip
+            {
+                Direction = direction //Ignoring all other properties
+            };
+
+            // act
+            var UpDirection = Models.Transformations.GetUpDirection(trip);
+
+            // assert
+            Assert.IsFalse(UpDirection, "Expecting value {0} from input of \"{1}\" but got {2} instead.", true, direction, UpDirection);
+
+        }
+
+        [TestMethod]
+        public void TestUpDirectionTransformationWithBadInput()
+        {
+            // arrange
+            var trip = new Models.HavmTrip
+            {
+                Direction = "all over the place" //Ignoring all other properties
+            };
+
+            // act
+            bool success;
+            try
+            {
+                var UpDirection = Models.Transformations.GetUpDirection(trip);
+                success = false;
+            }
+            #pragma warning disable CS0168 // Variable is declared but never used
+            catch (FormatException ex)
+            #pragma warning restore CS0168 // Variable is declared but never used
+            {
+                success = true; //we are expecting an exception when we pass invalid input
+            }
+
+            // assert
+            Assert.IsTrue(success, "Expecting a FormatException when passing invalid input. Didn't encounter such an exception.");
+        }
+        #endregion
+    }
+}
