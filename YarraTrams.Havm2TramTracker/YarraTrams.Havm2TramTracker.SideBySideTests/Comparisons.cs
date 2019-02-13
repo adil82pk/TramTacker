491fdb4b577a980c801eac051c92710dab254bb9
Anthony Daff <anthony@inoutput.io>
2019-02-13 15:06:19 +1100

    test: create T_Temp_SchedulesDetails comparison

--- YarraTrams.Havm2TramTracker/YarraTrams.Havm2TramTracker.SideBySideTests/Comparisons.cs
+++ YarraTrams.Havm2TramTracker/YarraTrams.Havm2TramTracker.SideBySideTests/Comparisons.cs
@@ -27,6 +27,10 @@ namespace YarraTrams.Havm2TramTracker.SideBySideTests
             schedulesMasterComparer.RunComparison(out DataTable existingRowsMissingFromNewT_Temp_SchedulesMaster, out DataTable newRowsNotInExistingT_Temp_SchedulesMaster, out DataTable existingRowsThatDifferFromNewT_Temp_SchedulesMaster);
             this.OutputToFile("T_Temp_SchedulesMaster", existingRowsMissingFromNewT_Temp_SchedulesMaster, newRowsNotInExistingT_Temp_SchedulesMaster, existingRowsThatDifferFromNewT_Temp_SchedulesMaster);
 
+            //T_Temp_schedulesDetails
+            var schedulesDetailsComparer = new Models.T_Temp_SchedulesDetailsComparer();
+            schedulesDetailsComparer.RunComparison(out DataTable existingRowsMissingFromNewT_Temp_SchedulesDetails, out DataTable newRowsNotInExistingT_Temp_SchedulesDetails, out DataTable existingRowsThatDifferFromNewT_Temp_SchedulesDetails);
+            this.OutputToFile("T_Temp_SchedulesDetails", existingRowsMissingFromNewT_Temp_SchedulesDetails, newRowsNotInExistingT_Temp_SchedulesDetails, existingRowsThatDifferFromNewT_Temp_SchedulesDetails);
         }
 
         private void OutputToFile(string tableName, DataTable existingRowsMissingFromNew, DataTable newRowsNotInExisting, DataTable existingRowsThatDifferFromNew)
