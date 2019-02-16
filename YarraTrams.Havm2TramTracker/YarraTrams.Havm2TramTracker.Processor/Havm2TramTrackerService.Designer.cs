3134ebd379c146c81bc335e7a8d343ca5182829a
Anthony Daff <anthony@inoutput.io>
2019-02-16 20:45:45 +1100

    chore: try configuring the installer again

--- YarraTrams.Havm2TramTracker/YarraTrams.Havm2TramTracker.Processor/Havm2TramTrackerService.Designer.cs
+++ YarraTrams.Havm2TramTracker/YarraTrams.Havm2TramTracker.Processor/Havm2TramTrackerService.Designer.cs
@@ -28,8 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.ServiceName = "Service1";
+            // 
+            // Havm2TramTrackerService
+            // 
+            this.ServiceName = "YarraTrams.Havm2TramTracker";
+
         }
 
         #endregion
