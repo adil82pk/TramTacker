f39090109d1e52bc9ff2d6342056d7c296844fee
Anthony Daff <anthony@inoutput.io>
2019-03-27 21:36:17 +1100

    feat: re-enable the saving to the T_Temp_SchedulesMaster/Details tables

--- YarraTrams.Havm2TramTracker/YarraTrams.Havm2TramTracker.Processor/Processor.cs
+++ YarraTrams.Havm2TramTracker/YarraTrams.Havm2TramTracker.Processor/Processor.cs
@@ -43,8 +43,8 @@ namespace YarraTrams.Havm2TramTracker.Processor
                 // Populate 4 temp tables
                 SaveToTrips(havmTrips);
                 SaveToSchedules(havmTrips);
-                //SaveToSchedulesMaster(havmTrips);
-                //SaveToSchedulesDetails(havmTrips);
+                SaveToSchedulesMaster(havmTrips);
+                SaveToSchedulesDetails(havmTrips);
             }
             catch (Exception ex)
             {
