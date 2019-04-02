886bae9ba338d68c62763eafda20a0a484db3dcb
Anthony Daff <anthony@inoutput.io>
2019-04-02 13:10:47 +1100

    chore: add core FluentMigrator dependancies and build scripts
    (No actual tables added yet.)

---
+++ YarraTrams.Havm2TramTracker/YarraTrams.Havm2TramTracker.TestComparisons/Migrations/201904011600_AddComparisonTables.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace YarraTrams.Havm2TramTracker.TestComparisons
+{
+    [Tags("TestComparison")]
+    [Migration(201904011600)]
+    public class DB_201904011600_AddComparisonTables : Migration
+    {
+        public override void Up()
+        {
+            
+        }
+
+        public override void Down()
+        {
+            
+        }
+    }
+}
