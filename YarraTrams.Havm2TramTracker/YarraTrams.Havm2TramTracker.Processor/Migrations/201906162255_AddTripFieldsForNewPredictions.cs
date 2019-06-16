56240e091d196219d8f297ae849a8103597b43de
Anthony Daff <anthony@inoutput.io>
2019-06-16 22:49:38 +1000

    feat: add DB fields to store new Havm2 trip data

---
+++ YarraTrams.Havm2TramTracker/YarraTrams.Havm2TramTracker.Processor/Migrations/201906162255_AddTripFieldsForNewPredictions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace YarraTrams.Havm2TramTracker.TestComparisons
+{
+    [Tags("TTBU")]
+    [Migration(201906162255)]
+    public class DB_201906162255_AddTripFieldsForNewPredictions : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("T_Trips").AddColumn("HavmTripId").AsInt32().NotNullable().SetExistingRowsTo(0);
+            Alter.Table("T_Trips").AddColumn("HavmTimetableId").AsInt32().NotNullable().SetExistingRowsTo(0);
+            Alter.Table("T_Trips").AddColumn("HastusPermanentTripNumber").AsInt32().NotNullable().SetExistingRowsTo(0);
+            Alter.Table("T_Trips").AddColumn("RunSequenceNumber").AsInt32().NotNullable().SetExistingRowsTo(0);
+            Alter.Table("T_Trips").AddColumn("HeadwayPreviousSeconds").AsInt32().NotNullable().SetExistingRowsTo(0);
+
+            Alter.Table("T_Temp_Trips").AddColumn("HavmTripId").AsInt32().NotNullable().SetExistingRowsTo(0);
+            Alter.Table("T_Temp_Trips").AddColumn("HavmTimetableId").AsInt32().NotNullable().SetExistingRowsTo(0);
+            Alter.Table("T_Temp_Trips").AddColumn("HastusPermanentTripNumber").AsInt32().NotNullable().SetExistingRowsTo(0);
+            Alter.Table("T_Temp_Trips").AddColumn("RunSequenceNumber").AsInt32().NotNullable().SetExistingRowsTo(0);
+            Alter.Table("T_Temp_Trips").AddColumn("HeadwayPreviousSeconds").AsInt32().NotNullable().SetExistingRowsTo(0);
+        }
+
+        public override void Down()
+        {
+            Delete.Column("HavmTripId").FromTable("T_Trips");
+            Delete.Column("HavmTimetableId").FromTable("T_Trips");
+            Delete.Column("HastusPermanentTripNumber").FromTable("T_Trips");
+            Delete.Column("RunSequenceNumber").FromTable("T_Trips");
+            Delete.Column("HeadwayPreviousSeconds").FromTable("T_Trips");
+
+            Delete.Column("HavmTripId").FromTable("T_Temp_Trips");
+            Delete.Column("HavmTimetableId").FromTable("T_Temp_Trips");
+            Delete.Column("HastusPermanentTripNumber").FromTable("T_Temp_Trips");
+            Delete.Column("RunSequenceNumber").FromTable("T_Temp_Trips");
+            Delete.Column("HeadwayPreviousSeconds").FromTable("T_Temp_Trips");
+        }
+    }
+}
