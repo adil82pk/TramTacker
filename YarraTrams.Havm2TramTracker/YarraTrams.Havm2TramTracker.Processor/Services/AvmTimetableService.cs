01b0b60c6c655be8d8c4af47553090072ee092ab
Anthony Daff <anthony@inoutput.io>
2020-11-11 22:38:10 +1100

    style: convert class-level constants to Pascal case.

--- YarraTrams.Havm2TramTracker/YarraTrams.Havm2TramTracker.Processor/Services/AvmTimetableService.cs
+++ YarraTrams.Havm2TramTracker/YarraTrams.Havm2TramTracker.Processor/Services/AvmTimetableService.cs
@@ -19,10 +19,10 @@ namespace YarraTrams.Havm2TramTracker.Processor.Services
         public readonly string AvmLogFileArchivePath;
         public readonly int AvmLogFileArchiveRetentionPeriodInDays;
 
-        private const string emptyFileErrorMessage = "AVM file is empty.";
-        private const string truncatedFileErrorMessage = "AVM file appears to be truncated.";
-        private const string tomorrowTimestampErrorMessage = "Expecting the second field on the third line of the file to be a timestamp";
-        private const string generalFileContentErrorMessage = "Unexpected format inside AVM file.";
+        private const string EmptyFileErrorMessage = "AVM file is empty.";
+        private const string TruncatedFileErrorMessage = "AVM file appears to be truncated.";
+        private const string TomorrowTimestampErrorMessage = "Expecting the second field on the third line of the file to be a timestamp";
+        private const string GeneralFileContentErrorMessage = "Unexpected format inside AVM file.";
 
         public AvmTimetableService()
         {
@@ -86,7 +86,7 @@ namespace YarraTrams.Havm2TramTracker.Processor.Services
             // If the file is empty we throw an exception.
             if (String.IsNullOrEmpty(content))
             {
-                throw new FormatException(emptyFileErrorMessage);
+                throw new FormatException(EmptyFileErrorMessage);
             }
 
             int timestamp = -1;
@@ -103,7 +103,7 @@ namespace YarraTrams.Havm2TramTracker.Processor.Services
                     // If the file appears to be truncated then we create an event but we continue (it's likely we have enough data to read tomorrow's timestamp).
                     if ((lineData.Length != 2 && lineData.Length != 8 && lineData.Length != 9) || (lineData.Length == 2 && lineData[1] != "NA"))
                     {
-                        LogWriter.Instance.Log(EventLogCodes.TRUNCATED_FILE_ON_AVM_ENDPOINT, $"{truncatedFileErrorMessage}\nLine {lineNumber}:{line}");
+                        LogWriter.Instance.Log(EventLogCodes.TRUNCATED_FILE_ON_AVM_ENDPOINT, $"{TruncatedFileErrorMessage}\nLine {lineNumber}:{line}");
                     }
 
                     // The timstamp for tomorrow's file is on line 3.
@@ -112,8 +112,8 @@ namespace YarraTrams.Havm2TramTracker.Processor.Services
                         // If the timestamp is not an integer then we create a specific event and abort (throw an exception).
                         if (!int.TryParse(lineData[5], out timestamp))
                         {
-                            LogWriter.Instance.Log(EventLogCodes.UNEXPECTED_FORMAT_INSIDE_AVM_FILE, tomorrowTimestampErrorMessage);
-                            throw new FormatException(tomorrowTimestampErrorMessage);
+                            LogWriter.Instance.Log(EventLogCodes.UNEXPECTED_FORMAT_INSIDE_AVM_FILE, TomorrowTimestampErrorMessage);
+                            throw new FormatException(TomorrowTimestampErrorMessage);
                         }
                     }
 
@@ -124,8 +124,8 @@ namespace YarraTrams.Havm2TramTracker.Processor.Services
             // If the timestamp never got set then we log a specific error then abort (throw an exception).
             if (timestamp < 0)
             {
-                LogWriter.Instance.Log(EventLogCodes.UNEXPECTED_FORMAT_INSIDE_AVM_FILE, generalFileContentErrorMessage);
-                throw new FormatException(generalFileContentErrorMessage);
+                LogWriter.Instance.Log(EventLogCodes.UNEXPECTED_FORMAT_INSIDE_AVM_FILE, GeneralFileContentErrorMessage);
+                throw new FormatException(GeneralFileContentErrorMessage);
             }
 
             return timestamp;
